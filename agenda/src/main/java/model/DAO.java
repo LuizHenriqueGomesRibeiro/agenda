8580f21550c7aa63de5f7444a6963c3876e077d8
LuizHenriqueGomesRibeiro <louisnuuk@hotmail.com>
2023-06-12 13:41:00 -0300

    comitar final do teste de conexão (sucesso)

--- agenda/src/main/java/model/DAO.java
+++ agenda/src/main/java/model/DAO.java
@@ -5,8 +5,8 @@ import java.sql.DriverManager;
 
 public class DAO {
 	private String driver = "com.mysql.cj.jdbc.Driver";
-	private String url = "jdbc:mysql://127.0.0.1:3306/agenda?useTimezone=true&serverTimezone=UTC";
-	private String user = "MySQL";
+	private String url = "jdbc:mysql://127.0.0.1:3306/dbagenda?useTimezone=true&serverTimezone=UTC";
+	private String user = "root";
 	private String passworld = "MySQL";
 
 	private Connection conectar(){
@@ -19,6 +19,17 @@ public class DAO {
 			// TODO: handle exception
 			System.out.println(e);
 			return null;
+		}
+	}
+	
+	public void teste() {
+		try {
+			Connection con=conectar();
+			System.out.println(con);
+			con.close();
+		} catch (Exception e) {
+			// TODO: handle exception
+			System.out.println(e);
 		}
 	}
 }
