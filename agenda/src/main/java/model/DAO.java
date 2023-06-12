61a75e723b1e587bf0d7834d774412a9282f5a28
LuizHenriqueGomesRibeiro <louisnuuk@hotmail.com>
2023-06-12 13:29:14 -0300

    comitar criação do método de conexão na classe DAO

--- agenda/src/main/java/model/DAO.java
+++ agenda/src/main/java/model/DAO.java
@@ -1,5 +1,24 @@
 package model;
 
-public class DAO{
+import java.sql.Connection;
+import java.sql.DriverManager;
 
+public class DAO {
+	private String driver = "com.mysql.cj.jdbc.Driver";
+	private String url = "jdbc:mysql://127.0.0.1:3306/agenda?useTimezone=true&serverTimezone=UTC";
+	private String user = "MySQL";
+	private String passworld = "MySQL";
+
+	private Connection conectar(){
+		Connection con=null;
+		try {
+			Class.forName(driver);
+			con=DriverManager.getConnection(url, user, passworld);
+			return con;
+		} catch (Exception e) {
+			// TODO: handle exception
+			System.out.println(e);
+			return null;
+		}
+	}
 }
