8afa808fd3e643c87fba5ab471b4b4684d65b486
LuizHenriqueGomesRibeiro <louisnuuk@hotmail.com>
2023-06-14 15:25:47 -0300

    comitar programa funcionando

--- agenda/src/main/webapp/script/jquery.js
+++ agenda/src/main/webapp/script/jquery.js
@@ -40,3 +40,9 @@ jQuery(function() {
 	}
 });
 
+function confirmar(idcon){
+	let resposta=confirm("Confirma a exclusão deste contato?")
+	if(resposta===true){
+		window.location.href="delete?idcon="+idcon
+		}
+}
